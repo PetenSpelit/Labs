416e8b75cf8be190954d5c601cc9a55cbbd53c15
C0mmunist1 <36222715+C0mmunist1@users.noreply.github.com>
2018-03-25 22:08:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab5
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			FoodBasket basket = new FoodBasket ();
+			Month mon = new Month ();
+
+			for (int i = 1; i < 12; i++) {
+				Console.WriteLine ("The price of a FoodBasket in " + mon.WhatMonth(i) + " is " + basket.PriceNow (i));
+			}
+		}
+	}
+}
