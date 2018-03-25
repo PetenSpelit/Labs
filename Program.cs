8c971d873975135397e57dd723694fa5932c66f6
C0mmunist1 <36222715+C0mmunist1@users.noreply.github.com>
2018-03-25 22:09:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab10
+{
+	class MainClass
+	{
+		public static void Main (String[] args)
+		{
+			int userInput;
+			Console.WriteLine ("Give height");
+			Int32.TryParse (Console.ReadLine (), out userInput);
+			Triangle tri = new Triangle (userInput);
+			tri.ChristmasTree ();
+		}
+	}
+}
---
+++ Triangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lab10
+{
+	public class Triangle
+	{
+		private int baseSize;
+
+		public Triangle (int size)
+		{
+			this.baseSize = size;
+		}
+
+		public void TriangleDraw ()
+		{
+			for (int i = 0; i < baseSize; i++) {
+				DrawStar (i);
+				Console.WriteLine ("");
+			}
+		}
+
+		public void ChristmasTree ()
+		{
+			int spaces = baseSize;
+			for (int j = 0; j < baseSize; j++) {
+				for (int i = 0; i < spaces; i++) {
+					Console.Write (" ");
+				}
+				DrawStar (j);
+				DrawStar (j-1);
+				Console.WriteLine ("");
+				spaces--;
+			}
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < baseSize - 1; j++) {
+					Console.Write (" ");
+				}
+				DrawStar (2);
+				Console.WriteLine ("");
+			}
+		}
+
+		private void DrawStar (int count)
+		{
+			for (int i = 0; i <= count; i++) {
+				Console.Write ("*");
+			}
+		}
+	}
+}
+
