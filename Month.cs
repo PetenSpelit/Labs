79f25d85de5f9fa08a1451106a66b21f1525e6a3
C0mmunist1 <36222715+C0mmunist1@users.noreply.github.com>
2018-03-25 22:07:23 +0300

    Add files via upload

---
+++ Month.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Lab6
+{
+	public class Month
+	{
+		private String january;
+		private String february;
+		private String march;
+		private String april;
+		private String may;
+		private String june;
+		private String july;
+		private String august;
+		private String september;
+		private String october;
+		private String november;
+		private String december;
+		private String month;
+
+		public Month ()
+		{
+			this.january = "January";
+			this.february = "February";
+			this.march = "March";
+			this.april = "April";
+			this.may = "May";
+			this.june = "June";
+			this.july = "July";
+			this.august = "August";
+			this.september = "September";
+			this.october = "October";
+			this.november = "November";
+			this.december = "December";
+			this.month = "";
+		}
+		public String WhatMonth(int monthnum)
+		{
+			switch(monthnum)
+			{
+			case 1:
+				month = january;
+				return month;
+			case 2:
+				month = february;
+				return month;
+			case 3:
+				month = march;
+				return month;
+			case 4:
+				month = april;
+				return month;
+			case 5:
+				month = may;
+				return month;
+			case 6:
+				month = june;
+				return month;
+			case 7:
+				month = july;
+				return month;
+			case 8:
+				month = august;
+				return month;
+			case 9:
+				month = september;
+				return month;
+			case 10:
+				month = october;
+				return month;
+			case 11:
+				month = november;
+				return month;
+			case 12:
+				month = december;
+				return month;
+			}
+			return month;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab6
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			FoodBasket basket = new FoodBasket ();
+			Month mon = new Month ();
+			double over = 0;
+			double price = 0;
+			for (int i = 1; i <= 12; i++) {
+				price = basket.PriceNow (i);
+				Console.WriteLine ("The price of a foodbasket in " + mon.WhatMonth(i) + " is " + price);
+				if (price > 105) {
+					over = price - 105;
+					Console.WriteLine ("The price of the foodbasket is over 105 euros by " + over + "\n");
+				}
+			}
+		}
+	}
+}
