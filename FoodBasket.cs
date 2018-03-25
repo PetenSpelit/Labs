9cfa02908458353c6546a9cb813d573d683e1d7a
C0mmunist1 <36222715+C0mmunist1@users.noreply.github.com>
2018-03-25 22:07:59 +0300

    Add files via upload

---
+++ FoodBasket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab6
+{
+	public class FoodBasket
+	{
+		private double price;
+		private double increase;
+		private double incEuro;
+
+		public FoodBasket ()
+		{
+			this.price = 100;
+			this.increase = 0.015;
+			this.incEuro = 0;
+		}
+		public double PriceNow(int monthnum)
+		{
+			increase = 0.015 * monthnum;
+			incEuro = price * increase;
+			price += incEuro;
+			return price;
+		}
+		public void Reset()
+		{
+			this.price = 100;
+			this.increase = 0.015;
+			this.incEuro = 0;
+		}
+	}
+}
+
---
+++ Month.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab6
+{
+	public class Month
+	{
+		private List<String> months;
+		private String monthReturn;
+
+		public Month ()
+		{
+			this.months = new List<String>()
+			{
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+				"August",
+				"September",
+				"October",
+				"November",
+				"December"
+			};
+			this.monthReturn = "";
+		}
+		public String WhatMonth(int monthnum)
+		{
+			monthReturn = months [monthnum];
+			return monthReturn;
+		}
+	}
+}
